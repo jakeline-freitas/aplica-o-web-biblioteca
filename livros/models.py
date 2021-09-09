27c107e101095c3000319817edd1347c82dcf25c
jakeline-freitas <jakefreaitas@gmail.com>
2021-09-09 18:34:02 -0300

    modelo do app livro

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=45)
+    genero = models.CharField(max_length=45)
+    qtd_paginas = models.IntegerField()
+
