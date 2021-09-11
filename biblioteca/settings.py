e8e6f27a97a645b83a943cfaa409b105a229d174
jakeline-freitas <jakefreaitas@gmail.com>
2021-09-10 21:31:57 -0300

    ajuste deploy

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import cast
 import django_on_heroku
 from decouple import config
 
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pjhf9r!fe2gi3)f5tk-m%4(^mrrxwluk9q9zk3tqmed9rvzloh"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
